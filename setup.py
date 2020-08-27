ea0ae18013d1d7d75550545c63607fb31871fd45
Johannes Haux <jo.mobile.2011@gmail.com>
2020-08-27 17:06:31 +0200

    :stars: initial commit
    This is a simple port from the edflow repo to extract some functionality
    that might be useful also outside of edflow.
    Checkout edflow here: github.com/pesser/edflow

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+def clean():
+    """Custom clean command to tidy up the project root."""
+    if "posix" in os.name:
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./*__pycache__/")
+    elif "nt" == os.name:
+        # possible to-do: also remove directories as above
+        os.system(r"del /s /q .\build .\dist .\*.pyc .\*.tgz")
+
+install_reqs = [
+    'numpy'
+]
+dev_reqs = [
+    'pytest',
+    'pytest-cov'
+]
+
+include_files = {}
+
+
+if __name__ == "__main__":
+
+    setup(
+        name="Walker",
+        version="0.1",
+        license="MIT",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        packages=find_packages(exclude=["tests", "test.*"]),
+        package_data=include_files,
+        install_requires=install_reqs,
+        extras_require={"dev": dev_reqs},
+        python_requires=">=3.7",
+    )
+
+    clean()
